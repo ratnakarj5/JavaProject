6a8c968d5cbc75561e8911684630b032e57e1741
jr_projects <j.ratnakar5@gmail.com>
2017-05-07 08:47:08 +0530

    Sysouts Added1
    Sysouts Added1

--- RestWebServices1/src/com/jr/webservices/HelloWorldService.java
+++ RestWebServices1/src/com/jr/webservices/HelloWorldService.java
@@ -11,6 +11,7 @@ public class HelloWorldService {
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
+		System.out.println("HelloWorldService : getMsg" + msg);
  
 		String output = "Jersey say : " + msg;
 		System.out.println("output : " + output);
